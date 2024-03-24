c46df9baa5c774542ab29ca3002666c95cb528b2
Georgios Kontos <georgios.t.kontos@gmail.com>
2024-03-24 15:11:37 +0100

    Changed data generation and reading data from csv file

--- DataGeneration.py
+++ DataGeneration.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 import csv
 
 def generate_continuous_data(T: int, min_val: float, max_val: float) -> np.array :
@@ -27,13 +28,21 @@ def generate_multiple_series(T: int, n: int, min_val: float, max_val: float, out
     """
     series: list = []
     
+    arr = np.zeros((n, T))
+    for i in range(n):
+        p: float = np.random.uniform(0, 0.1)
+        phi = np.random.uniform(0.7, 1)
+        ser = gen_AR1__missing(phi, T, p)
+        arr[i] = ser
+
+    scaled = scale_array(arr, min_val, max_val)
+    print
     with open(out, mode='w', newline='') as file:
         writer = csv.writer(file)
         for i in range(n):
-            p: float = np.random.uniform(0, 0.1)
-            y = generate_AR1_missing(T, 1, min_val, max_val, p)
-            series.append(y)
-            writer.writerow(y)
+            ser = scaled[i]
+            series.append(ser)
+            writer.writerow(ser)
 
     return series
 
@@ -45,36 +54,34 @@ def read_data(input: str) -> list:
     with open(input, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            time_series_list.append(np.array(row))
+            time_series_list.append(np.array(row, dtype=float))
 
     return time_series_list
 
-def scale_time_series(y: np.array, min_val: int, max_val: int) -> np.array:
-    """
-    Scale data between two values
-    """
-    curr_min: float = np.min(y)
-    curr_max: float = np.max(y)
+def scale_array(arr, a, b):
 
-    return min_val + ((y - curr_min) / (curr_max - curr_min)) * (max_val - min_val)
+    non_nan_mask = ~np.isnan(arr)
 
-def generate_AR1(T: int, param: float, min_val: int, max_val: int) -> np.array:
-    """
-    Generate a time series as a AR(1) process
-    """
-    y = np.zeros(T)
-    y[0] = np.random.normal(0, 1)
-    noise = np.random.normal(0, 0.25, T)
-    for i in range(1, T):
-        y[i] = param*y[i-1]+noise[i]
-    
-    return scale_time_series(y, min_val, max_val)
+    min_val = np.min(arr[non_nan_mask])
+    max_val = np.max(arr[non_nan_mask])
 
-def generate_AR1_missing(T: int, param: float, min_val: int, max_val: int, missing_prob: float) -> np.array:
-    """
-    Generate a time series as a AR(1) process with missing values
-    """
-    scaled_data = generate_AR1(T, param, min_val, max_val)
-    missing_mask: np.array = np.random.rand(T) < missing_prob
-    scaled_data[missing_mask] = np.nan
-    return scaled_data
+    scaled_non_nan = a + (arr[non_nan_mask] - min_val) * (b - a) / (max_val - min_val)
+
+    scaled_arr = np.empty_like(arr)
+    scaled_arr[non_nan_mask] = scaled_non_nan
+    scaled_arr[np.isnan(arr)] = np.nan
+
+    return scaled_arr
+
+
+
+def gen_AR1(phi, samples):
+    ar_process = sm.tsa.ArmaProcess(ar=np.r_[1, -phi], ma=np.array([1]))
+    ar_series = ar_process.generate_sample(nsample=samples)
+    return ar_series
+
+def gen_AR1__missing(phi, samples, p):
+    ar_series = gen_AR1(phi, samples)
+    missing_mask = np.random.rand(samples) < p
+    ar_series[missing_mask] = np.nan
+    return ar_series 
